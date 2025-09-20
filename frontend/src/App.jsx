a658f1d86911b9c4b9fd58d242f83cf80d96b36d
ZieksQ <nexisonytb@gmail.com>
2025-09-21 02:09:46 +0800

    new branch initial commit & added pages and navbar mockup design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from "./pages/HomePage.jsx";
 import PostCard from "./components/PostCard.jsx";
 import SamplePostCard from "./components/Sample.jsx";
 
-export default function App() {
+function App() {
   return (
     <>
       <HomePage />

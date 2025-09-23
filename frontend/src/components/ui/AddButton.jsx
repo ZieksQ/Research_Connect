23da991a31668940c00817303ba81ae8e4b1bdc7
ZieksQ <nexisonytb@gmail.com>
2025-09-23 18:38:04 +0800

    optimizing react reusable components | needs more tailwind and component optimization

--- frontend/src/components/ui/AddButton.jsx
+++ frontend/src/components/ui/AddButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+<<<<<<< HEAD
 // Add Button
 // Just for Add Button (Exclusive)
 // Add a Function to the add button (I'll do it later)
@@ -21,6 +22,21 @@ const AddButton = ({ func }) => {
       {/* Text */}
       <h3>Add</h3>
     </button>
+=======
+const AddButton = () => {
+  return (
+    <>
+      <button className="flex items-center gap-2">
+        {/* Logo */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="size-5 ">
+          <path d="M256 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 160-160 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l160 0 0 160c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160 160 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-160 0 0-160z" />
+        </svg>
+
+        {/* Text */}
+        <h3>Add</h3>
+      </button>
+    </>
+>>>>>>> b9e6eaf (optimizing react reusable components | needs more tailwind and component optimization)
   );
 };
 
--- frontend/src/components/ui/HambergerLinks.jsx
+++ frontend/src/components/ui/HambergerLinks.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 
 // This Component is for the Menus inside the Hamburger Menu
@@ -19,3 +20,22 @@ const HambergerLinks = ({ icon, name, func }) => {
 };
 
 export default HambergerLinks;
+=======
+import React from 'react'
+
+const HambergerLinks = ({icon, name, link}) => {
+  return (
+    <a href={link} className='flex items-center gap-2 rounded-md p-2 hover:bg-gray-200 active:bg-gray-300 '>
+        {/* Add New Button */}
+        <svg xmlns={icon.xmlns} viewBox={icon.viewBox} className={`size-${icon.size}`}>
+            <path d={icon.d}/>
+        </svg>
+        <span>{name}</span>
+    </a>
+
+    
+  )
+}
+
+export default HambergerLinks
+>>>>>>> b9e6eaf (optimizing react reusable components | needs more tailwind and component optimization)
